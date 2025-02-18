970557da1994badc3bb011e6e0544dcf102108c8
John A. Samuel <johnayobami77@proton.me>
2025-02-18 22:13:56 +0100

    Updated SEO

--- app/resources/notes-and-PQs/layout.tsx
+++ app/resources/notes-and-PQs/layout.tsx
@@ -2,18 +2,18 @@ import Head from "next/head";
 import { ReactNode } from "react";
 
 export const metadata = {
-  title: "About EkoStudy - Our Mission & Vision",
+  title: "Resources EkoStudy - Our Mission & Vision",
   description:
-    "Learn about EkoStudy’s mission to empower LASUSTECH students with top-quality academic resources, interactive learning tools, and expert study support.",
+    "Learn Resources EkoStudy’s mission to empower LASUSTECH students with top-quality academic resources, interactive learning tools, and expert study support.",
   keywords:
-    "About EkoStudy, EkoStudy mission, LASUSTECH education platform, student learning resources, academic support, study tools, online education, university success, learning innovation",
+    "Resources EkoStudy, EkoStudy mission, LASUSTECH education platform, student learning resources, academic support, study tools, online education, university success, learning innovation",
 };
 
-type AboutLayoutProps = {
+type ResourcesLayoutProps = {
   children: ReactNode;
 };
 
-export default function Layout({ children }: AboutLayoutProps) {
+export default function Layout({ children }: ResourcesLayoutProps) {
   return (
     <html lang="en">
       <Head>
@@ -26,14 +26,14 @@ export default function Layout({ children }: AboutLayoutProps) {
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://ekostudy.vercel.app/about" />
-        <meta property="og:image" content="https://ik.imagekit.io/mshcgnjju/EkoStudy/about-banner.png" />
+        <meta property="og:url" content="https://ekostudy.vercel.app/Resources" />
+        <meta property="og:image" content="https://ik.imagekit.io/mshcgnjju/EkoStudy/Resources-banner.png" />
 
         {/* Twitter Meta Tags */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content={metadata.title} />
         <meta property="twitter:description" content={metadata.description} />
-        <meta property="twitter:image" content="https://ik.imagekit.io/mshcgnjju/EkoStudy/about-banner.png" />
+        <meta property="twitter:image" content="https://ik.imagekit.io/mshcgnjju/EkoStudy/Resources-banner.png" />
       </Head>
       <body>
         {children}
