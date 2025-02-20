95bea2916db10800b8b8cf3bc84b9c01616f4068
John A. Samuel <johnayobami77@proton.me>
2025-02-20 20:51:49 +0100

    updated products config

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -2,18 +2,18 @@ import Head from "next/head";
 import { ReactNode } from "react";
 
 export const metadata = {
-  title: "Contact Us - EkoStudy",
+  title: "Products Us - EkoStudy",
   description:
     "Get in touch with EkoStudy for support, inquiries, or feedback. We are here to help LASUSTECH students with all their academic needs.",
   keywords:
-    "contact EkoStudy, LASUSTECH support, student inquiries, academic assistance, help desk, educational support",
+    "Products EkoStudy, LASUSTECH support, student inquiries, academic assistance, help desk, educational support",
 };
 
-type ContactLayoutProps = {
+type ProductsLayoutProps = {
   children: ReactNode;
 };
 
-export default function ContactLayout({ children }: ContactLayoutProps) {
+export default function ProductsLayout({ children }: ProductsLayoutProps) {
   return (
     <html lang="en">
       <Head>
@@ -26,14 +26,14 @@ export default function ContactLayout({ children }: ContactLayoutProps) {
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://ekostudy.vercel.app/contact" />
-        <meta property="og:image" content="https://ik.imagekit.io/mshcgnjju/EkoStudy/contact-banner.png" />
+        <meta property="og:url" content="https://ekostudy.vercel.app/Products" />
+        <meta property="og:image" content="https://ik.imagekit.io/mshcgnjju/EkoStudy/Products-banner.png" />
 
         {/* Twitter Meta Tags */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content={metadata.title} />
         <meta property="twitter:description" content={metadata.description} />
-        <meta property="twitter:image" content="https://ik.imagekit.io/mshcgnjju/EkoStudy/contact-banner.png" />
+        <meta property="twitter:image" content="https://ik.imagekit.io/mshcgnjju/EkoStudy/Products-banner.png" />
       </Head>
       <body>
         {children}
