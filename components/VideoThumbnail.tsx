48e4f20a6afa744fb88f03514baf313a32a696f1
John A. Samuel <johnayobami77@proton.me>
2025-02-26 16:00:39 +0100

    Updates

--- components/VideoThumbnail.tsx
+++ components/VideoThumbnail.tsx
@@ -1,31 +1,36 @@
-const getYouTubeVideoId = (url: string) => {
-    const match = url.match(/(?:youtu\.be\/|youtube\.com\/(?:.*v=|.*\/|embed\/|v\/))([\w-]{11})/);
-    return match ? match[1] : null;
-  };
-  
-  const VideoThumbnail = ({ video, isActive, onClick }) => {
-    const videoId = getYouTubeVideoId(video);
-    const thumbnailUrl = videoId ? `https://img.youtube.com/vi/${videoId}/0.jpg` : "";
-  
-    return (
-      <div
-        className={`cursor-pointer border-2 rounded-lg overflow-hidden transition-transform transform ${
-          isActive ? "border-green-500 scale-110" : "border-gray-300"
-        }`}
-        onClick={onClick}
-      >
-        {videoId ? (
-          <img
-            src={thumbnailUrl}
-            alt="Video Thumbnail"
-            className="w-full h-20 object-cover"
-          />
-        ) : (
-          <p className="p-4 text-red-700 text-sm font-normal text-center">Video not available yet...</p>
-        )}
-      </div>
-    );
-  };
-  
+interface VideoThumbnailProps {
+  video: string;
+  isActive: boolean;
+  onClick: () => void;
+}
 
-  export default VideoThumbnail;
+const getYouTubeVideoId = (url: string): string | null => {
+  const match = url.match(/(?:youtu\.be\/|youtube\.com\/(?:.*v=|.*\/|embed\/|v\/))([\w-]{11})/);
+  return match ? match[1] : null;
+};
+
+const VideoThumbnail: React.FC<VideoThumbnailProps> = ({ video, isActive, onClick }) => {
+  const videoId = getYouTubeVideoId(video);
+  const thumbnailUrl = videoId ? `https://img.youtube.com/vi/${videoId}/0.jpg` : "";
+
+  return (
+    <div
+      className={`cursor-pointer border-2 rounded-lg overflow-hidden transition-transform transform ${
+        isActive ? "border-green-500 scale-110" : "border-gray-300"
+      }`}
+      onClick={onClick}
+    >
+      {videoId ? (
+        <img
+          src={thumbnailUrl}
+          alt="Video Thumbnail"
+          className="w-full h-20 object-cover"
+        />
+      ) : (
+        <p className="p-4 text-red-700 text-sm font-normal text-center">Video not available yet...</p>
+      )}
+    </div>
+  );
+};
+
+export default VideoThumbnail;
