645a70378e6123ab8e9453fb4c866e638bf97e92
John A. Samuel <johnayobami77@proton.me>
2025-03-18 04:35:08 +0100

    Bug fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Facebook, Twitter, Youtube, Phone, Mail } from 'lucide-react';
+import { Facebook, Twitter, Youtube, Mail } from 'lucide-react';
 import FooterBanner from './FooterBanner';
 
 const Footer: React.FC = () => {
@@ -24,7 +24,7 @@ const Footer: React.FC = () => {
                         </div>
                         <div className="flex flex-col gap-6 w-full">
                             <h3 className="text-[#92B76D] font-bold text-base">OUR OFFICE</h3>
-                            <p>EkoStudy, 104101 Ikorodu, <br /> Lagos.</p>
+                            <p>EkoStudy, Ikorodu, <br /> Lagos.</p>
                             <div className='flex flex-col gap-1 mt-2 lg:mt-0'>
                                 
                                 <Link href="mailto:ekostudy-73bec1@smtpexpress.email" className="flex flex-row items-center">
@@ -61,7 +61,7 @@ const Footer: React.FC = () => {
                 </div>
                 <hr className='mx-10 xl:mx-24 mb-2 h-[1px] border-[#4C5F4E] bg-[#4C5F4E]' />
                 <div className='px-10 xl:px-24 mt-4 flex flex-col lg:flex-row gap-2 lg:gap-0 items-start lg:items-center justify-between'>
-                    <span>EkoStudy 2024 - <Link href="https://github.com/codegallantx" target="_blank" className="text-[#4C5F4E] hover:text-[#FFCA0D] transition-all duration-300 ease-in-out">CodeGallantX</Link></span>
+                    <span>EkoStudy 2025 - <Link href="https://github.com/codegallantx" target="_blank" className="text-[#4C5F4E] hover:text-[#FFCA0D] transition-all duration-300 ease-in-out">CodeGallantX</Link></span>
                     <span className="flex flex-row gap-2 lg:gap-4">
                         <Link href="#" className="hover:text-[#FFCA0D] transition-all duration-300 ease-in-out">Privacy Policy</Link>
                         <span className='text-[#FFCA0D] text-lg font-thin'>|</span>
